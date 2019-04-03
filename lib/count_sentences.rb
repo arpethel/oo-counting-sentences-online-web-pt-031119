40723e03595870c6296326dfe7d00a1e2a40fee2
Ryan Pethel <arpethel@gmail.com>
2019-04-03 09:25:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -22,64 +22,11 @@ class String
       return 0
     else
       sentence_array.each do |word|
-        # binding.pry
         count += 1 if word.sentence?
         count += 1 if word.question?
         count += 1 if word.exclamation?
       end
       return count
     end
-
-
-    # sentence_array.each do |phrase|
-    #   if phrase.sentence?
-    #     sentences << phrase
-    #   end
-    #   if phrase.question?
-    #     sentences << phrase
-    #   end
-    #   if phrase.exclamation?
-    #     sentences << phrase
-    #   end
-    #     # count = sentences.count
-    #   if sentences.count > 0
-    #     return sentences.count
-    #   else
-    #     return 0
-    #   end
-
-    #   if phrase.sentence?
-    #     if phrase[-1] == "." && phrase[-2] != "." #|| (phrase[-1] == "?")
-    #       if sentence_array.empty?
-    #         return 0
-    #       else
-    #       sentences << phrase
-    #       end
-    #     else
-    #       return 0
-    #     # number_of_sentences = sentences.count
-    #     end
-    #   elsif phrase.question?
-    #     if phrase[-1] == "?" && phrase[-2] != "?" #|| (phrase[-1] == "?")
-    #       if sentence_array.empty?
-    #         return 0
-    #       else
-    #       sentences << phrase
-    #       end
-    #     else
-    #       return 0
-    #     # number_of_sentences = sentences.count
-    #     end
-    #   elsif phrase.exclamation?
-    #
-    #   end
-    #
-    #
-    # return number_of_sentences
-    # binding.pry
-    # return sentences.count
-    # end
-    # binding.pry
-    # return count
   end
 end
